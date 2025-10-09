cc1742349bf98c5044b5279ab3826a49019991f5
santosh <santosh.leanq@gmail.com>
2025-10-09 17:02:27 +0545

    popups

--- js/app.js
+++ js/app.js
@@ -3,12 +3,23 @@ import { NewEnquiryModal } from "./models/new-enquiry.js";
 import { NewEnquiryView } from "./views/new-enquiry.js";
 import { initDashboard } from "./controller/dashboard.js";
 
+// import { JobDetailController } from "./controller/job-detail.js";
+// import { JobDetailModal } from "./models/job-detail.js";
+// import { JobDetailView } from "./views/job-detail.js";
+
 document.addEventListener("DOMContentLoaded", () => {
-  // initDashboard();
+  initDashboard();
+  // let modal = new NewEnquiryModal();
+  // let view = new NewEnquiryView(modal);
+  // let controller = new NewEnquiryController(modal, view);
+  // controller.init();
 
-  let modal = new NewEnquiryModal();
-  let view = new NewEnquiryView(modal);
-  let controller = new NewEnquiryController(modal, view);
+  // let jobDetailModal = new JobDetailModal();
+  // let jobDetailView = new JobDetailView(jobDetailModal);
+  // let jobDetailController = new JobDetailController(
+  //   jobDetailModal,
+  //   jobDetailView
+  // );
 
-  controller.init();
+  // jobDetailController.init();
 });
---
+++ js/controller/job-detail.js
@@ -0,0 +1,28 @@
+export class JobDetailController {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+    this.init();
+  }
+
+  init() {
+    this.onDealInfoButtonClicked();
+    this.onEditBtnClicked();
+  }
+
+  onDealInfoButtonClicked() {
+    let dealInfoBtn = document.getElementById("deal-info-btn");
+    dealInfoBtn.addEventListener("click", (event) => {
+      this.view.toggleDealInformation();
+    });
+  }
+
+  onEditBtnClicked() {
+    let editBtn = document.querySelectorAll("#edit-note-btn");
+    editBtn.forEach((item) => {
+      item.addEventListener("click", (event) => {
+        this.view.toggleTasksModal();
+      });
+    });
+  }
+}
